405b69c358f157f74b0c0e6ec4fa3759153ed917
vijayakabajo <vijayakabajo@gmail.com>
2025-04-15 18:32:59 +0530

    merging mesh

--- index.js
+++ index.js
@@ -26,15 +26,32 @@ camera.position.y = 0.5; //positioning camera a lil higher to see things at the 
 //can move camera with mouse
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+controls.dampingFactor = 0.02;
 
 //Adding object/mesh to the scene
 // const geometry = new THREE.BoxGeometry();
 
 
-const geometry = new THREE.SphereGeometry(1, 40, 40);  //radius, widthSegments, heightSegments
-const material = new THREE.MeshBasicMaterial({ color: 0x1e90ff, wireframe: true });
+const geometry = new THREE.SphereGeometry(1, 30, 30);  //radius, widthSegments, heightSegments
+const material = new THREE.MeshStandardMaterial({ color: 0xffffff, wireframe: true}); //color, flatShading
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+
+const wiremat = new THREE.MeshBasicMaterial({ color: 0xffff0f, wireframe: true });  //color, wireframe
+const wiremesh = new THREE.Mesh(geometry, wiremat); 
+// scene.add(wiremesh);
+cube.add(wiremesh); //adding wireframe to the cube so that it moves with the cube
+wiremesh.position.set(0, 0, 0); //positioning wireframe to the center of the cube
+
+const wiremat2 = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });  //color, wireframe
+const wiremesh2 = new THREE.Mesh(geometry, wiremat2);
+cube.add(wiremesh2); //adding wireframe to the cube so that it moves with the cube
+wiremesh2.position.set(0, 0, 0); //positioning wireframe to the center of the cube
+wiremesh2.scale.set(1.7, 1.7, 1.7); //to make the wireframe bigger than the cube
+
+const light  = new THREE.HemisphereLight(0x0000ff, 0xff00bb); //color, groundColor, intensity
+scene.add(light);
 
 //to make it Responsive
 window.addEventListener('resize', () => {
@@ -45,10 +62,12 @@ window.addEventListener('resize', () => {
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x +=0.003;
-  cube.rotation.y += 0.004;
+  cube.rotation.x +=0.002;
+  cube.rotation.y -= 0.02;
+  wiremesh.scale.set(1.4, 1.4, 1.4); //to make the wireframe bigger than the cube
     // cube.rotation.z += 0.005;
-  controls.update();
+    // wireframe.scale.set(1.01, 1.01, 1.01); //to make the wireframe bigger than the cube
+    controls.update(); //to make the camera move with mouse
   renderer.render(scene, camera);
 }
 animate();
